4a4708c42b067f6fe35191944f797fda26a4a9fe
tmerca <amercadal20233@iesjoanramis.org>
2023-10-04 19:52:04 +0200

    04_10_2023_UD03

---
+++ UD03/script.js
@@ -0,0 +1,20 @@
+/*
+    ESRUCTURAS DE CONTROL
+
+    Se utilizan para controlar el flujo de un programa
+*/
+
+
+/*
+
+    if
+
+    Se utilitza para ejecutar un bloque de codigo
+    si se cumple una condición
+
+*/
+
+let edad = 18;
+let esMayorDeEdad = (edad >= 18);
+
+if (esMayorDeEdad) console.log("Eres mayor de edad");
