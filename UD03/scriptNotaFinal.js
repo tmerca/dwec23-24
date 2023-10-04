2f0579db398210141ca29491d0820a6318297070
tmerca <amercadal20233@iesjoanramis.org>
2023-10-04 20:58:57 +0200

    Exercici NotaFinal + Comentaris

--- UD03/scriptNotaFinal.js
+++ UD03/scriptNotaFinal.js
@@ -3,6 +3,7 @@ let notaExamen1 = 7.90;
 let notaExamen2 = 6.80;
 let notaProjecte = 6.77;
 
+//Variable que hace el cálculo para saber la nota final con los porcentajes indicados en el enunciado
 let notaFinal = (notaExamen1 * 0.2) + (notaExamen2 * 0.2) + (notaProjecte * 0.6);
 
 console.log("Nom de l'alumne: " + nomEstudiant);
@@ -12,6 +13,8 @@ console.log("Nota del projecte: " + notaProjecte);
 console.log("");
 console.log("Nota final: " + notaFinal);
 
+
+//Condicional que muestra mensaje dependiendo de la nota final del alumno
 if(notaFinal >= 7){
     console.log("Enhorabona " + nomEstudiant + " has aprovat el mòdul!");
 }else{
