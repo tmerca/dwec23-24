369768bb5a2f27c5b548389eb4bc4cee61d049b2
tmerca <amercadal20233@iesjoanramis.org>
2023-10-09 21:26:15 +0200

    apuntes

---
+++ UD04/script.js
@@ -0,0 +1,45 @@
+// Arrays
+
+// Definir arrays
+
+//let vacio = []; //No tiene elementos
+//let primos = [2, 3, 5, 7, 11]; //Array con 5 elementos numéricos
+//let miscelanea = [1.1, true, "a", ]; //Varios tipos y coma by the face
+
+// Los valores de los elementos no tienen porque ser constantes
+
+// let n = 1024;
+// let tabla = [n, n + 1, n + 2, n + 3];
+
+// let contador = [,,,,];
+// console.log(contador.length);
+
+
+// OPERADOR Spread
+// let a = [1, 2, 3];
+// let b = [0, ...a ,4];
+// console.log(b);
+// console.log(b.length);
+
+// let original = [1, 2, 3];
+// let copia = [...original];
+// original[0] = 9;
+// console.log(original[0]);
+// console.log(copia[0]);
+
+// let digitos = [..."0123456789ABCDEF"];
+// console.log(digitos);
+
+// Constructor de array
+
+//let a = new Array(10); //Array con 10 elementos
+//let b = Array.of(10); //Array Of 10 le pone el valor 10
+//console.log("Longitud de a " + a.length);
+//console.log(a);
+//console.log("Longitud de b " + b.length)
+//console.log(b);;
+//console.log(Array.of(10));
+
+// METODO PUSH (añade el valor al final del array)
+//a.push(5);
+//console.log(a[a.length-1]);
