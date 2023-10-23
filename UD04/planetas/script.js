477217114bf8131db7e65df065ea2c2cd2ccb79e
tmerca <amercadal20233@iesjoanramis.org>
2023-10-23 21:04:06 +0200

    Ejercicio Planetas

--- UD04/planetas/script.js
+++ UD04/planetas/script.js
@@ -1,14 +1,14 @@
 //Array con los nombres de los planetas que he elegido
-let nombrePlaneta= ["Mercurio", "Venus", "Marte", "Jupiter", "Urano"];
+let nombrePlaneta= ["Jupiter", "Urano", "Marte", "Mercurio", "Venus"];
 // El array distanciaAlSol está en Km
-let distanciaAlSol = [58000000, 108000000, 228000000, 750000000, 3000000000];
+let distanciaAlSol = [750000000, 3000000000, 228000000,58000000, 108000000];
 // Número de planeta Tierra que se necesitan para igualar el diámetro
-let tamañoRelativo = [0.38, 0.86, 2, 1321, 63];
+let tamañoRelativo = [0.38, 63, 2, 1321, 0.86];
 
 let fecha = new Date();
-let fechaDescubrimiento = [fecha.getDate(), fecha.getTime(), fecha.getTime(), fecha.getTime(), fecha.getTime()];
+let fechaDescubrimiento = [fecha.getTime(), fecha.getTime(), fecha.getTime(), fecha.getTime(), fecha.getTime()];
 
-//CONSTRUCTOR DE PLANETAS
+// CONSTRUCTOR DE PLANETAS
 // ponemos las propiedades necesarias para luego coger los datos del array y tambien
 // una funcion para retornar toda la información del Objeto
 function Planeta(){
@@ -27,12 +27,20 @@ function Planeta(){
 let planetas = [];
 
 // Bucle For para poder rellenar el array vacío llamado planetas y
-// y que muestre por pantalla la informacion de cada objeto
 for(i = 0; i < distanciaAlSol.length; i++){
     planetas[i] = new Planeta();
     planetas[i].nombrePlaneta = nombrePlaneta[i];
     planetas[i].distanciaAlSol = distanciaAlSol[i];
     planetas[i].tamañoRelativo = tamañoRelativo[i];
     planetas[i].fechaDescubrimiento = fechaDescubrimiento[i];
+}
+
+// Método para ordenar los planetas por distancia al sol
+planetas.sort((a,b) => {
+    return a.distanciaAlSol - b.distanciaAlSol;
+});
+
+// Bucle para mostrar la información de los planetas ordenados por distancia
+for(i = 0; i < planetas.length; i++){
     console.log(planetas[i].muestraInformacion());
 }
